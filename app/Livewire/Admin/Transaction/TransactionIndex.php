ffcdb00eb85dc646b1dfbdbe49bd723395d935d6
maulana677 <maull0776@gmail.com>
2024-03-07 16:13:08 +0700

    fix fitur batalkan transaksi

--- app/Livewire/Admin/Transaction/TransactionIndex.php
+++ app/Livewire/Admin/Transaction/TransactionIndex.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire\Admin\Transaction;
 
+use App\Models\Product;
 use App\Models\Transaction;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class TransactionIndex extends Component
 {
+    public function delete($id)
+    {
+        $transaction = Transaction::find($id);
+        $product = Product::find($transaction->product_id);
+        $product->update([
+            'stock' => $product->stock + $transaction->quantity
+        ]);
+
+        $transaction->delete($id);
+        session()->flash('status', 'Transaksi Berhasil Dibatalkan');
+    }
+
     #[Layout('layouts.admin')]
     public function render()
     {
